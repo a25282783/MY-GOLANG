7f8072fbbfaba14317c954d3798c975d5faf1c20
Louis Liu <lliu@elta.com.tw>
2023-01-13 16:52:10 +0800

    reflect

--- reflect_demo/main.go
+++ reflect_demo/main.go
@@ -19,8 +19,25 @@ func foo(any any /*任何東西*/) {
 	fmt.Println(refV.Interface().(int) + 2) // 12 轉成空接口再斷言
 }
 
+func getKind(any interface{} /*簡寫*/) {
+	refT := reflect.TypeOf(any)
+
+	fmt.Println(refT.Kind())
+}
+
 func main() {
-	int1 := 10
-	foo(int1)
+	// int1 := 10
+	// foo(int1)
+	// getKind(int1)
+
+	type A struct {
+	}
+	a := &A{}
+	getKind(*a) // struct
+	b := &A{}
+	getKind(b) // ptr
+
+	slice1 := []int{0, 1}
+	getKind(slice1) // slice
 
 }
